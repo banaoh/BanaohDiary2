9fb3a0f8ad8bf403d10b86f49810d7be034a484b
Kengo Obana <aoyama.banaoh@gmail.com>
2016-12-06 09:58:45 +0000

    最終週のcommit

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles do
+    resource :likes, only: [:create, :destroy]
+  end
 end
